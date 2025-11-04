d01458d8e1d0aa045ddf71506055a34187270c08
Kelly-ML <kelymeza@hotmail.com>
2025-11-04 20:06:08 -0300

    Update test_NumParMeza.py

--- tests/test_NumParMeza.py
+++ tests/test_NumParMeza.py
@@ -1,5 +1,6 @@
  #tests/test_es_par.py
 from funciones.NumparMeza import es_par
+
 def test_es_par():
-assert es_par(4) is True
-assert es_par(7) is False
+ assert es_par(4) is True
+ assert es_par(7) is False
