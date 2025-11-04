e92a99ea9e9649dc2bb91b01092a7267300e2082
emanuel valverde <105718974+yuskema@users.noreply.github.com>
2025-11-04 20:23:12 -0300

    Update test_sumaguzman.py

--- tests/test_sumaguzman.py
+++ tests/test_sumaguzman.py
@@ -1,5 +1,5 @@
 from funciones.sumaguzman import sumar
 
-def test_es_par():
-    assert es_par(4) is True    # 4 es par, debería devolver True
-    assert es_par(7) is False   # 7 es impar, debería devolver False
+def test_suma():
+    assert suma(4,5) is True    # 4 es par, debería devolver True
+    assert suma(7,5) is False   # 7 es impar, debería devolver False
