658c54f83d96d3acb371329e3a505a0c3a9c56ad
nazarenarodriguez <nazarenarodriguez108@gamil.com>
2025-11-05 01:48:03 -0300

    corrijo validacion de tipo en factorial_rodriguez

--- funciones/factorial_rodriguez.py
+++ funciones/factorial_rodriguez.py
@@ -2,6 +2,6 @@ import math
 
 def factorial(n):
     """Devuelve el factorial de un número entero no negativo."""
-    if n < 0:
+    if not isinstance(n, int) or n < 0:
         return None
     return math.factorial(n)
